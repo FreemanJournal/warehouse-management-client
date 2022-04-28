bd8f1445043ac0f54ee564db047aae1127037668
Md Ishaque <iamishaque.business@gmail.com>
2022-04-29 01:05:24 +0600

    create biolerplate

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from 'react';
+import FileBase64 from 'react-file-base64';
+import { createItem, getItems } from './utilities/functions';
 function App() {
+  const [item, setItem] = useState({ title: '', image: '' });
+  const [items, setItems] = useState([])
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    const result = await createItem(item);
+    setItems([...items, result]);
+  }
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await getItems();
+      console.log('fetch data;m', result)
+      setItems(result)
+    }
+    fetchData()
+  }, [])
   return (
-    <div className="App">
-  {/* git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/ProgrammingHeroWC4/warehouse-management-client-side-FreemanJournal.git
-git push -u origin main */}
+    <div className="container">
+      <pre>{JSON.stringify(item, null, '\t')}</pre>
+      <form action="" onSubmit={onSubmitHandler}>
+        <input type="text" className="input-field"
+          onChange={e => setItem({ ...item, title: e.target.value })}
+        />
+        <FileBase64
+          type="file"
+          multiple={false}
+          onDone={({ base64 }) => setItem({ ...item, image: base64 })}
+        />
+        <div className="right-align">
+          <button className="btn">submit</button>
+        </div>
+      </form>
+      {items?.map(item => (
+        <div className="card" key={item._id}>
+          <div className="card-image waves-effect waves-block waves-light">
+            <img className="activator" style={{ width: '100%', height: 300 }} src={item.image} alt="ItemPhoto"/>
+          </div>
+          <div className="card-content">
+            <span className="card-title activator grey-text text-darken-4">{item.title}</span>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
-
 export default App;
---
+++ src/components/UploadNewProducts.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function UploadNewProducts() {
+  return (
+    <div>UploadNewProducts</div>
+  )
+}
